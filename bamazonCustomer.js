10df0bf629a61e60b410a5f60b7caed0099f2e98
Lazaro <Lazaro.Perez0823@gmail.com>
2019-11-26 17:17:38 -0500

    Final for now

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,82 +1,101 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var spacer = "\n" + "*-------------------------------------------------------------------------------------------------------------------------------------*" + "\n"
 
 var connection = mysql.createConnection({
     host: "localhost",
-
-    // Your port; if not 3306
     port: 3306,
-
-    // Your username
     user: "root",
-
-    // Your password
     password: "password",
     database: "bamazonDB"
 });
 
-connection.connect(function (err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
-    ListItems();
-});
+function runAgain() {
 
-function ListItems() {
+    inquirer.prompt([
+    {        
+    type: "list",
+    name: "again",
+    message: "Do you wish to purchase something else?",
+    choices: ["YES", "NO"]
+    }
+]).then(function (answer) {
+        if (answer.again == "YES") {
+        listItems();
+        }
+        
+        else {
+        connection.end();
+        }
+    })
+
+};
+
+
+function listItems() {
 
     connection.query("SELECT * FROM Products", function (err, res) {
-        console.log(res);
         if (err) throw err;
-        console.log("\n" + "Thank you for coming to Bamazon for all your shopping needs" + "\n\n" + "Current Inventory" + "\n")
+        console.log(spacer + "Thank you for coming to Bamazon for all your shopping needs" + "\n\n" + "Current Inventory")
         res.forEach(stuff => {
             console.log("Item ID: " + stuff.item_id + " | " + "Product Name: " + stuff.product_name + " | " + "Department Name: " + stuff.department_name + " | " + "Price: " + stuff.price);
-                })
-                console.log("\n")
+        })
+        console.log(spacer)
 
-            PromptAction();
+        promptAction();
     })
 };
 
-function PromptAction() {
-    inquirer.prompt([
-        {
-        name: "pickItem",
-        type: "number",
-        message: "What is the ID of the Product you would like to purchase?"
-      },
+function promptAction() {
+    inquirer.prompt([{
+                name: "pickItem",
+                type: "number",
+                message: "What is the ID of the Product you would like to purchase?"
+            },
 
-      {
-        name: "howMany",
-        type: "number",
-        message: "How many would you like to purchase?"
-      }])
-    //   .then(function(answer) {
-    //     var query = "SELECT position, song, year FROM top5000 WHERE ?";
-    //     connection.query(query, { artist: answer.artist }, function(err, res) {
-    //       if (err) throw err;
-    //       for (var i = 0; i < res.length; i++) {
-    //         console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Year: " + res[i].year);
-    //       }
-    //       runSearch();
-    //     });
-    //   });
-  }
+            {
+                name: "howMany",
+                type: "number",
+                message: "How many would you like to purchase?"
+            }
+        ])
 
+        .then(function (answer) {
+            var query = "SELECT item_id, price, stock_quantity FROM products WHERE ?";
+            connection.query(query, {
+                    item_id: answer.pickItem
+                },
+                (err, results) => {
 
+                    if (results.length === 0) {
+                        console.log(spacer + "Product ID NOT found. Going back to item selection" + spacer);
 
+                    runAgain();
 
+                    } else if (answer.howMany <= results[0].stock_quantity) {
+                        var total = answer.howMany * results[0].price;
+                        var query = connection.query("UPDATE products SET ? WHERE ?", [{
+                                stock_quantity: results[0].stock_quantity - answer.howMany
+                            },
+                            {
+                                item_id: answer.pickItem
+                            }
+                        ], (err, res) => {
+                            if (err) throw err;
+                            console.log(spacer + "Transaction was successful. Total cost: " + total);
+                            runAgain();
+                        });
 
+                    } else {
+                        console.log(spacer + "There's only " + results[0].stock_quantity + " available. Our supply doesn't meet your demand. \n Please select a quantity equal to or lower than our current stock\n" + spacer);
+                       runAgain();
+                    }
+                });
+        });
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    listItems();
+})
